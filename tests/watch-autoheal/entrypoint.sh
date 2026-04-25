28f05a792e642eb8750d0214a33ec703fdcca98e
Serhiy Bobrov <sergey.bobrov83@gmail.com>
2026-04-25 11:57:17 +0300

    Add support for restarting stopped or crashed containers
    - Enhanced the autoheal functionality to monitor and restart stopped or crashed containers based on the specified label.
    - Updated README.md to reflect the new behavior and usage instructions.
    - Added test cases in docker-compose.yml to validate the new functionality.
    - Modified entrypoint.sh to handle Docker events for stopped containers.

--- tests/watch-autoheal/entrypoint.sh
+++ tests/watch-autoheal/entrypoint.sh
@@ -4,15 +4,23 @@ set -euxo pipefail
 listenToDockerEvents()
 {
   local expected_restarts
+  local keep_restarting_seen
+  local keep_restarting_stopped_seen
   local LOGLINE
   expected_restarts=0
-  docker events --filter 'com.docker.compose.service=should-keep-restarting' --filter 'com.docker.compose.service=shouldnt-restart-*' --filter 'event=restart' | while read -r LOGLINE
+  keep_restarting_seen=0
+  keep_restarting_stopped_seen=0
+
+  docker events --filter 'event=restart' | while read -r LOGLINE
   do
     echo "$LOGLINE"
+    [[ $LOGLINE != *"com.docker.compose.project=$COMPOSE_PROJECT_NAME"* ]] && continue
+
     # may be more elaborate checks here.
-    [[ $LOGLINE == *"container restart "*"com.docker.compose.service=shouldnt-restart-"* && $LOGLINE == *"com.docker.compose.project=$COMPOSE_PROJECT_NAME"* ]] && echo "ERR: No restarts expected on shouldnt-restart-* containers!" 1>&2 && pkill -9 docker && exit 1
-    [[ $LOGLINE == *"container restart "*"com.docker.compose.service=should-keep-restarting"* && $LOGLINE == *"com.docker.compose.project=$COMPOSE_PROJECT_NAME"* ]] && echo "OK: Expected restart on should-keep-restarting container!" && pkill -9 docker && expected_restarts=$((expected_restarts + 1))
-    [[ $expected_restarts == 1 ]] && echo "OK: All expected restarts happened" && exit 0
+    [[ $LOGLINE == *"com.docker.compose.service=shouldnt-restart-"* ]] && echo "ERR: No restarts expected on shouldnt-restart-* containers!" 1>&2 && pkill -9 docker && exit 1
+    [[ $LOGLINE == *"com.docker.compose.service=should-keep-restarting "* && $keep_restarting_seen -eq 0 ]] && echo "OK: Expected restart on should-keep-restarting container!" && keep_restarting_seen=1 && expected_restarts=$((expected_restarts + 1))
+    [[ $LOGLINE == *"com.docker.compose.service=should-keep-restarting-stopped "* && $keep_restarting_stopped_seen -eq 0 ]] && echo "OK: Expected restart on should-keep-restarting-stopped container!" && keep_restarting_stopped_seen=1 && expected_restarts=$((expected_restarts + 1))
+    [[ $expected_restarts == 2 ]] && echo "OK: All expected restarts happened" && pkill -9 docker && exit 0
   done
 }
 
